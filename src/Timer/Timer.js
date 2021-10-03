1107eabc4e6969f5005917bd99e2a47b80dbc9bf
Crystal_1831 <43097573+Crystak1831@users.noreply.github.com>
2021-10-03 13:29:41 -0400

    change Timer

--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -1,59 +1,90 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Timer() {
-  const [minutes, setMinutes] = useState(0);
+export default function App() {
+  const [mins, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const [isActive, setActive] = useState(false);
-
-  // 因为
-  useEffect(() => {
-    let timer;
-    if (isActive) {
-      timer = setTimeout(() => {
-        if (seconds < 59) {
-          setSeconds(seconds + 1);
-        } else {
-          setMinutes(minutes + 1);
-          setSeconds(0);
-        }
-      }, 1000);
-    } else if (!isActive) {
-      clearTimeout(timer);
-    }
+  const [time, setTime] = useState({
+    minutes: parseInt(mins),
+    seconds: parseInt(seconds)
   });
 
-  const handleChange1 = (e) => {
+  const [start, setStart] = useState(false);
+  const [pause, setPause] = useState(false);
+  const [over, setOver] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      if (pause) {
+        return;
+      }
+      if (time.minutes === 0 && time.seconds === 0) {
+        setOver(true);
+      } else if (time.seconds === 0) {
+        setTime({
+          minutes: time.minutes - 1,
+          seconds: 59
+        });
+      } else {
+        setTime({
+          minutes: time.minutes,
+          seconds: time.seconds - 1
+        });
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  });
+
+  const handleMin = (e) => {
     setMinutes(e.target.value);
   };
-  const handleChange2 = (e) => {
+
+  const handleSec = (e) => {
     setSeconds(e.target.value);
   };
 
-  const handleStart = () => {
-    setActive(!isActive);
+  const startHandler = () => {
+    setTime({
+      minutes: seconds > 60 ? parseInt(mins) + 1 : parseInt(mins),
+      seconds: seconds > 60 ? parseInt(seconds) - 60 : parseInt(seconds)
+    });
+    setStart(!start);
   };
 
-  const handlePause = () => {
-    setActive(false);
+  const pauseHandler = () => {
+    setPause(!pause);
   };
 
-  const handleReset = () =>{
-    setMinutes(0);
-    setSeconds(0);
-  }
+  const resetHandler = () => {
+    setTime({
+      minutes: seconds > 60 ? parseInt(mins) + 1 : parseInt(mins),
+      seconds: seconds > 60 ? parseInt(seconds) - 60 : parseInt(seconds)
+    });
+    setPause(false);
+    setStart(false);
+    setOver(false);
+  };
 
   return (
-    <div>
-      <span>minutes</span>
-      <input type="number" value={minutes} onChange={handleChange1} />
-      <span>seconds</span>
-      <input type="number" value={seconds} onChange={handleChange2} />
+    <div className="App">
       <div>
-        <button onClick={handleStart}>start</button>
-        <button onClick={handlePause}>pause</button>
-        <button onClick={handleReset}>reset</button>
+        <label>
+          Minutes:
+          <input type="number" onChange={handleMin} />
+        </label>
+        <label>
+          Seconds:
+          <input type="number" onChange={handleSec} />
+        </label>
+        <button onClick={startHandler}>start</button>
+        <button onClick={pauseHandler}>Pause</button>
+        <button onClick={resetHandler}>Reset</button>
+        <h1>
+          {`${time.minutes
+            .toString()
+            .padStart(2, "0")}:${time.seconds.toString().padStart(2, "0")}`}
+        </h1>
       </div>
-      {minutes}:{seconds}
     </div>
   );
 }
