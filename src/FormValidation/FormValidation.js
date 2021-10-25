3fe0f90f857d1323e7be581ca4956afb7e95e987
Crystal_1831 <43097573+Crystak1831@users.noreply.github.com>
2021-10-25 15:43:39 -0400

    add css style

--- src/FormValidation/FormValidation.js
+++ src/FormValidation/FormValidation.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import "./FormValidation.css";
 
 export default function FormValidation() {
   const [formValue, setFormValue] = useState("");
   const [inputLsit, setInputList] = useState([]);
+  const [enterValid, setEnterValid] = useState(true);
   // const [show, setShow] = useState(false);
 
   const handleForm = (e) => {
@@ -21,17 +23,29 @@ export default function FormValidation() {
     // };
     // setShow(!show);
     // console.log(newItem);fd
+    if (formValue.trim() === "") {
+      setEnterValid(false);
+      return;
+    }
+
+    setEnterValid(true);
     setInputList([...inputLsit, formValue]);
     setFormValue("");
     console.log(inputLsit);
   };
 
+  const nameInputClasses = enterValid ? "form-control" : "form-control invalid";
+
   return (
     <div>
       <form onSubmit={handleForm}>
-        <label>Enter the Name</label>
-        <input type="text" onChange={(e) => onChange(e)} value={formValue} />
-        <button onClick={handleSubmit}>onSubmit</button>
+        <div className={nameInputClasses}>
+          <label>Enter the Name</label>
+          <input type="text" onChange={(e) => onChange(e)} value={formValue} />
+          <button onClick={handleSubmit}>onSubmit</button>
+          {!enterValid && <p className="error-text">must enter the name</p>}
+        </div>
+
         {/* {show ? formValue : ""} */}
       </form>
       {inputLsit.length > 0 &&
